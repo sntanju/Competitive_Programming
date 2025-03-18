b06806d564768c6098ea982e1b95cfee8f78c167
Sanjida Nasrin Tanju <86721558+sntanju@users.noreply.github.com>
2025-03-18 08:45:07 +0600

    updated

---
+++ CodeForcesCode/A. Twins.cpp
@@ -0,0 +1,79 @@
+///   ***   ---            In the name of ALLAH        |||   ---   ***   ///
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu () {            cerr << endl;}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+
+int main()
+{
+    optimize();
+    /// START
+
+    ll n;
+    cin >> n;
+
+    ll sum = 0, ans = 0, p = 0;
+    ll a[n];
+
+    for(ll i = 0; i < n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+
+    sum /= 2;
+    sort(a, a + n);
+
+    for(ll i = n - 1; i >= 0; i--) {
+        ans += a[i];
+
+        p++;
+        if(ans > sum) break;
+    }
+
+    cout << p;
+
+    return 0;
+}
