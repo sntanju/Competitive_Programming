990a581014aee6a07db60cf02eef2cbef2035e01
Sanjida Nasrin Tanju <86721558+sntanju@users.noreply.github.com>
2025-03-23 09:21:12 +0600

    updated

---
+++ CodeForcesCode/A. Serval and String Theory.cpp
@@ -0,0 +1,84 @@
+///   ***   ---            In the name of ALLAH        |||   ---   ***   ///
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu () {            cerr << endl;}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+
+int main()
+{
+    optimize();
+    /// START
+
+    ll t;
+    cin >> t;
+
+    while(t--) {
+
+        ll n, k;
+        cin >> n >> k;
+
+        string s;
+        cin >> s;
+
+        string temp = s;
+        reverse(temp.begin(), temp.end());
+
+        ll cnt = 0;
+        if(s < temp) cnt = 0;
+
+        else {
+
+            sort(s.begin(),s.end());
+            if(s[0] == s[n - 1]) cnt = k + 1;
+            else cnt = 1;
+        }
+
+        if(cnt > k) cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+
+    return 0;
+}
